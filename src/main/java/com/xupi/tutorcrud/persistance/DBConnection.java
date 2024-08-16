d567b32d3f8a1572d8c0d48a8f6f7c594e087f45
SantherEHPM <erickhernandop@gmail.com>
2024-08-15 21:36:24 -0500

    BD Connection

---
+++ src/main/java/com/xupi/tutorcrud/persistance/DBConnection.java
@@ -0,0 +1,52 @@
+
+package com.xupi.tutorcrud.persistance;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class DBConnection {
+    private String user;
+    private String password;
+    private String dataBase;
+    private String server;
+    private String port;
+    private String strConnection;
+    private Connection con;
+    private static DBConnection instance;
+    
+    private DBConnection(){
+        this.user = "root";
+        this.password = "12345";
+        this.dataBase = "comarcas_xupi";
+        this.server = "localhost";
+        this.port = "3306";
+        this.strConnection = "jdbc:mysql//:"+this.server+":"+this.port+"/"+this.dataBase;
+        this.con = null;
+    }
+    
+    private Connection connectionToDB(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.con = DriverManager.getConnection(this.strConnection,this.user,this.password);
+            JOptionPane.showMessageDialog(null, "Conexion exitosa");
+        }catch(Exception e){
+            System.out.print(e);
+            JOptionPane.showMessageDialog(null, "Conexion inexitosa");   
+        }
+        
+        return this.con;
+    }
+    
+    public Connection getConnectionToBD(){
+        return this.connectionToDB();
+    }
+    
+    //Sigleton
+    public static DBConnection get(){
+        if(instance == null){
+            instance = new DBConnection();
+        }
+        return instance;
+    }
+}
