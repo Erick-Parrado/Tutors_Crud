9af19f29263aa83319ea0d06eb9d394f8eafec42
SantherEHPM <erickhernandop@gmail.com>
2024-08-15 22:59:11 -0500

    cosas

---
+++ src/main/java/logic/TutorLogic.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logic;
+
+import com.xupi.tutorcrud.persistance.TutorDTO;
+
+/**
+ *
+ * @author erick
+ */
+public class TutorLogic extends TutorDTO{
+    
+}
